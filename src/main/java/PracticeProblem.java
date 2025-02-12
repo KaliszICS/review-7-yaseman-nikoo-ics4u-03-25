c54b0653fca55c1151db67c6b625f9efc9b6a854
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-12 20:15:36 +0000

    t

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,65 @@ public class PracticeProblem {
 
 	}
 
-	//Write your functions here
 
-}
+	public static String evenOrOdd(int num){
+		if (num%2==0){
+		return "Even";
+
+		}
+		else{
+		return "Odd";
+		}
+
+	public static String teacherOrStudent(String word){
+		if (word.equals("Kalisz")){
+			return "Teacher";
+		}
+		else{
+			return "Student";
+		}
+	}
+
+	public static int farhterFromZero(int num){
+		if (num<0){
+			return num-5;
+		}
+		else if(num>0){
+			return num+5;
+		}
+		else{
+			return num;
+		}
+	}
+	public static String isFive(int num){
+		if (num == 5){
+			return "The number is Five";
+		}
+		else{
+			return "The number is not Five";
+		}
+	}
+	public static String positiveOrNegative(double num){
+		if (num >0){
+			return "Positive";
+		}
+		else{
+			return "Neagaive";
+		}
+	}
+	public static String highOrLow(int num){
+		if (num > 100){
+			return "High";
+		}
+		else{
+			return "Low";
+		}
+	}
+	public static String isHello(String word){
+		if (word.equals("Hello")){
+			return "The word is Hello";
+		}
+		else{
+			return "The word is not Hello";
+		}
+	}
